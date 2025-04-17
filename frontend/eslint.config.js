71e453125de6c1c0034786fd15606625030b85ac
Zøzo <benbar2412@gmail.com>
2025-04-17 10:32:51 +0200

    Initialisation ViteJS

---
+++ frontend/eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react: react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+
+      ...prettier.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
