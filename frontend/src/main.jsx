8fcc364b2ad719170c398f50586625f982e0d677
Zøzo <benbar2412@gmail.com>
2025-04-17 10:49:30 +0200

    feat: Réorganisation des dossiers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.jsx';
+import App from './pages/App.jsx';
 
-import './App.css';
-import './index.css';
+import './css/normalize.css';
+import './scss/index.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
